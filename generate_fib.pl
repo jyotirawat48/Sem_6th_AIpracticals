0342bdec7223d0dc3fb9f93251a54dd1ea8176f7
jyotirawat48 <72351536+jyotirawat48@users.noreply.github.com>
2022-05-01 18:31:14 +0530

    Create generate_fib.pl

---
+++ generate_fib.pl
@@ -0,0 +1,11 @@
+fibonacci(1,0).
+fibonacci(2,1).
+fibonacci(N,X):-
+    N > 2,
+    N1 is N-1,
+    N2 is N-2,
+    fibonacci(N1,X1),
+    fibonacci(N2,X2),
+    X is X1+X2.
+#fibonacci(4,X).
+#X=2
